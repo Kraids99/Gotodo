bf4d1ac6e25395075c0092341cf0a0ed7b82bb45
Kraids99 <alexanderwongso99@gmail.com>
2026-01-08 19:35:48 +0700

    update be + fe structure

--- api/controllers/controllers.go
+++ api/controllers/controllers.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"gotodo/database"
 	"gotodo/models"
+	"strings"
 	"net/http"
 	"strconv"
 
@@ -86,13 +87,20 @@ func UpdateGotodo(c *gin.Context) {
 	}
 
 	var updateData struct {
-		Title     string `json:"title"`
-		Completed bool   `json:"completed"`
+		Title string `json:"title"`
+		Completed bool `json:"completed"`
 	}
 
 	if err := c.ShouldBindJSON(&updateData); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error(),
+		})
+		return
+	}
+
+	if strings.TrimSpace(updateData.Title) == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "title cannot be empty",
 		})
 		return
 	}
